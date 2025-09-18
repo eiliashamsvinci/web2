38f37604d1f6fc5f5690eb695ecda01fb4dab996
Eilia <eilia.shams@student.vinci.be>
2025-09-18 14:21:41 +0200

    Exercice 1.2 terminé

--- web2/tutorials/back/api/basic/app.ts
+++ web2/tutorials/back/api/basic/app.ts
@@ -1,11 +1,24 @@
 import express  from "express";
 
+
 import usersRouter from "./routes/users";
 import pizzaRouter from "./routes/pizzas";
 import drinkRouter from "./routes/drinks";
 import filmRouter from "./routes/films" ; 
 
 const app = express();
+
+let counter = 0;
+
+
+
+app.use((req , _res , next)=>{
+    if (req.method === "GET"){
+        counter++ ; 
+        console.log( " GET counter :  " + counter) ; 
+    }
+    next() ; 
+}) ; 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
